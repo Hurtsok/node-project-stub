b9c8726a864188d3ac01981cd4970546552519d5
hurtsok <hurtsok@gmail.com>
2015-01-13 08:38:56 +1100

    add routes, samples
    its not a full commit

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ var express = require('express'),
     app  = express(),
     swig = require('swig'),
     router = require(__dirname + '/config/routes')(express),
+    stylus = require('stylus'),
     server;
 
 
+//register template engine
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/templates');
+
+app.set('staticPath', '/static');
 app.use(router);
+
+app.use('/css', function(req, res, next){
+    var str = require('fs').readFileSync(__dirname + app.get('staticPath') + req.originalUrl, 'utf8');
+    next();
+})
+
+//static url
+app.use(express.static(__dirname + app.get('staticPath')));
 
 server = app.listen(3000, function () {
     console.log('Server is running at port ' + server.address().port);
--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,7 @@ module.exports = function(express){
     var router = express.Router();
 
     router.get('/', function(req, res){
-        res.render('index.html')
+        res.render('index.html', { base: req.protocol + '://' + req.hostname  + ':3000'})
     });
 
     return router;
